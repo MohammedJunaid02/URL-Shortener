f41040a7e78483f079c8995dc4b73fa64fa42bcd
Mohammed Junaid <mohammedjunaidj08@gmail.com>
2024-05-12 01:10:05 +0530

    Added Code for Statless Authentication using JSON WEB TOKENS

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,35 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secretKey = "MJunaid$@8954";
+function setUser(user){
+    //stateless authentication
+    // const payload = {
+    //     _id: user._id,
+    //     email: user.email,
+    // };
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+    return jwt.sign(
+        {
+            _id: user._id,
+            email: user.email,
+        }, 
+        secretKey
+    );
+
+    // stateful authentication
+    // sessionIdToUserMap.set(id,user);
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    // return sessionIdToUserMap.get(id);
+
+    if (!token) return null;
+    try{
+        return jwt.verify(token, secretKey);
+    }
+    catch(error){
+        return null;
+    }
 }
 
 module.exports = {
