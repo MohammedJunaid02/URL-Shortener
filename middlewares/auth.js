ca8aa000b84359d5ffd22252f4ac98ebffac688e
Mohammed Junaid <mohammedjunaidj08@gmail.com>
2024-05-12 17:49:02 +0530

    Added Authentication in Header using Bearer Authentication - Header

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,47 @@
 const { getUser } = require("../service/auth");
 
 async function restrictToLoggedInUserOnly (req,res,next){
-    const userUid = await req.cookies.uid;
+    //bearer authentication--header
+    const userUid = req.headers["authorization"];
     if(!userUid) return res.redirect("/logIn");
 
-    const user = getUser(userUid);
+    const token = userUid.split("Bearer ")[1];
+    const user = getUser(token);
     if(!user) return res.redirect("/logIn");
 
     req.user = user;
     next();
+
+
+    // const userUid = await req.cookies.uid;
+    // if(!userUid) return res.redirect("/logIn");
+
+    // const user = getUser(userUid);
+    // if(!user) return res.redirect("/logIn");
+
+    // req.user = user;
+    // next();
 }
 
 async function checkAuthentication(req,res,next){
-    const userUid = await req.cookies.uid;
-    
-    const user = getUser(userUid);
+    const userUid = req.headers["authorization"];
 
-    req.user = user;
+    //bearer authentication--header
+    if(userUid)
+    {
+        const token = userUid.split("Bearer ")[1];
+        const user = getUser(token);
+        req.user = user;
+    }
     next();
+
+
+    // const userUid = await req.cookies.uid;
+    
+    // const user = getUser(userUid);
+
+    // req.user = user;
+    // next();
 }
 
 module.exports = {
